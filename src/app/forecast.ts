0d2073a048b21c13f4a089a1bc27b5281310e2d2
SulFa-X <grellety.raphael@gmail.com>
2020-05-08 19:11:14 +0200

    appmeteo

--- src/app/forecast.ts
+++ src/app/forecast.ts
@@ -5,3 +5,18 @@ export interface Forecast {
     day: number;
     weather: number;
 }
+
+export interface City {
+    insee: string;
+    cp: number;
+    name: string;
+    latitude: number;
+    longitude: number;
+    altitude: number;
+}
+
+export interface WeatherResponse {
+    city: City;
+    update: string;
+    forecast: Forecast[];
+}
